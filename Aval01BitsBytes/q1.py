d461ac91af122792fde5c84c23fbd1d688f845c8
PJordan2998 <pedro.jordan@escolar.ifrn.edu.br>
2025-05-05 23:14:56 -0300

    update1.1

---
+++ Aval01BitsBytes/q1.py
@@ -0,0 +1,6 @@
+ip = input("Digite o IPv4 (ex: 192.168.1.100): ").strip()
+bits = int(input("Digite a máscara em bits (ex: 24): ").strip())
+
+ip_num = 0
+for parte in ip.split('.'):
+    ip_num = (ip_num << 8) + int(parte)
