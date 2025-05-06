2d68ad118a36150927c78453d81a39a4072123a7
PJordan2998 <pedro.jordan@escolar.ifrn.edu.br>
2025-05-06 19:18:43 -0300

    Update 1.2 q1.py

--- Aval01BitsBytes/q1.py
+++ Aval01BitsBytes/q1.py
@@ -1,6 +1,8 @@
+#Entrada do número ip e máscara da rede#
 ip = input("Digite o IPv4 (ex: 192.168.1.100): ").strip()
 bits = int(input("Digite a máscara em bits (ex: 24): ").strip())
 
 ip_num = 0
 for parte in ip.split('.'):
     ip_num = (ip_num << 8) + int(parte)
+
