d809451df2b18513155e56e09e7521e378b752e7
PJordan2998 <pedro.jordan@escolar.ifrn.edu.br>
2025-05-06 23:05:26 -0300

    update 1.4

--- Aval01BitsBytes/q1.py
+++ Aval01BitsBytes/q1.py
@@ -4,3 +4,7 @@ bits = int(input("Digite a máscara em bits (ex: 24): ").strip())
 ip_num = 0
 for parte in ip.split('.'):
     ip_num = (ip_num << 8) + int(parte)
+
+mascara = (0xFFFFFFFF << (32 - bits)) & 0xFFFFFFFF if bits > 0 else 0
+
+
