23bfc3e999df319661793a3aaf3c4e6e8ec13459
PJordan2998 <pedro.jordan@escolar.ifrn.edu.br>
2025-05-11 18:04:00 -0300

    Update 2.8 q2.py

--- Aval01BitsBytes/q2.py
+++ Aval01BitsBytes/q2.py
@@ -1,15 +1,17 @@
 import time, hashlib, struct
 
-def contar_bits_zero_iniciais(bytes_hash):
-    contador = 0
-    for byte in bytes_hash:
-        if byte == 0:
-            contador += 8
-        else:
-            
-            for i in range(7, -1, -1):
-                if (byte >> i) & 1:
-                    break
-                contador += 1
-            break
-    return contador
+def principal():
+    tabela = [("Esse um texto elementar", 8),
+        ("Esse um texto elementar", 10),
+        ("Esse um texto elementar", 15),
+        ("Textinho", 8),
+        ("Textinho", 18),
+        ("Textinho", 22),
+        ("Meu texto médio", 18),
+        ("Meu texto médio", 19),
+        ("Meu texto médio", 20)]
+    
+    print(f"{'Texto a validar':<25} {'Bits em zero':<10} {'Nonce':<15} {'Tempo (em s)':<12}")
+    
+    for texto, bits in tabela:
+        dados = texto.encode('utf-8')
