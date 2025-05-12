2fedcb20d5e71686bc4bbfac0de8517fd0d65a6c
PJordan2998 <pedro.jordan@escolar.ifrn.edu.br>
2025-05-11 23:57:07 -0300

    Update 3.1 q3.py

--- Aval01BitsBytes/q3.py
+++ Aval01BitsBytes/q3.py
@@ -1,3 +1,9 @@
-arquivo_jpeg = 'foto.jpg'
+Imagem = ''
 
-with open(arquivo_jpeg, 'rb') as f:
+try:
+    with open(imagem, 'rb') as f:
+        leitura = f.read(6)
+      
+        if leitura [0] != 0xFF or leitura [1] != 0xD8:
+            print("Erro: Arquivo não é um JPEG válido")
+            exit()
