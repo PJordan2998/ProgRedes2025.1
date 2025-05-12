a1bb5ced1d1debba00c091dcd2b11ee6ed07ecf0
PJordan2998 <pedro.jordan@escolar.ifrn.edu.br>
2025-05-12 11:58:51 -0300

    Update 3.5 q3.py

--- Aval01BitsBytes/q3.py
+++ Aval01BitsBytes/q3.py
@@ -1,9 +1,15 @@
-Imagem = ''
+# Abre a imagem JPEG e lê os primeiros 6 bytes #
+with open('IMG_20250509_184205.jpg', 'rb') as imagem:
+    leia = imagem.read(6)
 
-try:
-    with open(imagem, 'rb') as f:
-        leitura = f.read(6)
-      
-        if leitura [0] != 0xFF or leitura [1] != 0xD8:
-            print("Erro: Arquivo não é um JPEG válido")
-            exit()
+# Extrai o app1DataSize das posições 4 e 5 (2 bytes) #
+app1DataSize = (leia[4] << 8) + leia[5]
+
+# Fecha #
+imagem.close()
+
+# Reabre a imagem e ignora os primeiros 4 bytes #
+with open('IMG_20250509_184205.jpg', 'rb') as imagem:
+    imagem.read(4) 
+# Lê app1DataSize bytes para app1Data #
+    app1Data = imagem.read(app1DataSize)
