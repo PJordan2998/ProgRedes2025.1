b6a368e3104769363fbe96b4439208a74b93d444
PJordan2998 <pedro.jordan@escolar.ifrn.edu.br>
2025-06-15 20:26:21 -0300

    0.4 update

--- Aval02Packets/q1.py
+++ Aval02Packets/q1.py
@@ -20,3 +20,27 @@ if len(sys.argv) < 2:
 
 # pega o arquivo informado #
 nome_arquivo = sys.argv[1]
+
+# Abre o arquivo pcap em modo leitura binária #
+with open(nome_arquivo, 'rb') as arquivo_pcap:
+# Lê e descarta o cabeçalho global do pcap 24 bytes #
+    arquivo_pcap.read(24)  
+
+    while True:
+# Lê o cabeçalho de cada pacote 16 bytes #
+        cabecalho_pacote = arquivo_pcap.read(16)  
+        if len(cabecalho_pacote) < 16:
+            break 
+
+# retira informações do cabeçalho do pacote timestamp e tamanho #
+        tempo_seg, tempo_usec, tam_incluido, tam_original = struct.unpack('=IIII', cabecalho_pacote)
+
+# Lê os dados do pacote #
+        dados_pacote = arquivo_pcap.read(tam_incluido)
+
+        if len(dados_pacote) < 14:
+# Ignora pacotes menores que o cabeçalho Ethernet #
+            continue  
+
+        print("\n" + "=" * 70)
+        print("Pacote lido ({} bytes)".format(len(dados_pacote)))
