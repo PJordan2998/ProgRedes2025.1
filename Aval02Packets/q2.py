ee57de5882defaf7948291170c9423405ececc8c
PJordan2998 <pedro.jordan@escolar.ifrn.edu.br>
2025-06-15 20:22:20 -0300

    0.3 update

--- Aval02Packets/q2.py
+++ Aval02Packets/q2.py
@@ -18,3 +18,18 @@ with open(caminho_imagem, 'rb') as arquivo_imagem:
 if b'Exif' not in conteudo:
     print("Erro: Dados EXIF não encontrados na imagem.")
     sys.exit()
+
+inicio_dados_exif = conteudo.find(b'Exif') + 6
+dados_exif = conteudo[inicio_dados_exif:]
+
+# Define a ordem dos bytes #
+cabecalho = dados_exif[:2]
+modo_little_endian = cabecalho == b'II'
+
+# Função para leitura de inteiros #
+if modo_little_endian:
+    ler_16bits = lambda b, o: b[o] + (b[o+1] << 8)
+    ler_32bits = lambda b, o: b[o] + (b[o+1] << 8) + (b[o+2] << 16) + (b[o+3] << 24)
+else:
+    ler_16bits = lambda b, o: (b[o] << 8) + b[o+1]
+    ler_32bits = lambda b, o: (b[o] << 24) + (b[o+1] << 16) + (b[o+2] << 8) + b[o+3]
