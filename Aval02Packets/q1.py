ba0dbc7be4de1fc872642c7cd11dc2469d884b73
PJordan2998 <pedro.jordan@escolar.ifrn.edu.br>
2025-06-15 21:43:18 -0300

    0.5 update

--- Aval02Packets/q1.py
+++ Aval02Packets/q1.py
@@ -44,3 +44,16 @@ with open(nome_arquivo, 'rb') as arquivo_pcap:
 
         print("\n" + "=" * 70)
         print("Pacote lido ({} bytes)".format(len(dados_pacote)))
+
+# Lê mac de destino e origem 6 bytes #
+        mac_destino = ':'.join('{:02x}'.format(b) for b in dados_pacote[0:6])   
+        mac_origem = ':'.join('{:02x}'.format(b) for b in dados_pacote[6:12])  
+# Extrai o tipo do protocolo 2 bytes # 
+        tipo_ethernet = struct.unpack('!H', dados_pacote[12:14])[0]     
+# Separa o restante para a camada de rede #        
+        dados_rede = dados_pacote[14:]                                          
+
+        print("[Camada Enlace - Ethernet]")
+        print(f"  ➜ MAC de Origem : {mac_origem}")
+        print(f"  ➜ MAC de Destino: {mac_destino}")
+        print(f"  ➜ Tipo Ethernet : {tipo_ethernet:#06x}")
