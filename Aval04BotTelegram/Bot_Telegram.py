eeb841fa78cd6dd36dfe2989e624c20832a20be8
PJordan2998 <pedro.jordan@escolar.ifrn.edu.br>
2025-08-12 13:22:01 -0300

    0.2

--- Aval04BotTelegram/Bot_Telegram.py
+++ Aval04BotTelegram/Bot_Telegram.py
@@ -15,9 +15,17 @@ def obter_atualizacoes(token, offset=None):
     url = f'https://api.telegram.org/bot{token}/getUpdates'
     parametros = {'timeout': 3, 'offset': offset}
     try:
-        resposta = requests.get(url, params=parametros, timeout=15)
+        resposta = requests.get(url, params=parametros, timeout=3)
         resposta.raise_for_status()
         return resposta.json()
     except Exception as erro:
         print(f"Erro ao obter atualizações: {erro}")
         return {}
+
+def enviar_mensagem(token, id_chat, texto):
+    url = f'https://api.telegram.org/bot{token}/sendMessage'
+    dados = {'chat_id': id_chat, 'text': texto}
+    try:
+        requests.post(url, data=dados, timeout=3)
+    except Exception as erro:
+        print(f"Erro ao enviar mensagem: {erro}")
